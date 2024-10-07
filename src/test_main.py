5f8368a135a4892f3a39ab25baaa85e19e92af30
liu149 <liuqi1212@outlook.com>
2024-10-07 23:37:42 +0800

    fix test

--- src/test_main.py
+++ src/test_main.py
@@ -16,13 +16,3 @@ def test_health():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
-
-def test_read_item():
-    response = client.get("/items/foo", params={"q": "bar"})
-    assert response.status_code == 200
-    assert response.json() == {"item_id": "foo", "q": "bar"}
-
-def test_cpu_task():
-    response = client.get("/cpu-task/1")
-    assert response.status_code == 200
-    assert "CPU intensive task completed for 1 seconds" in response.json()["message"]
